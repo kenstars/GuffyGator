62a59e432990a750a547fde7251082e2e887a305
Kannan <kannan@userworkstech.com>
2016-12-06 18:25:08 +0530

    Gator

---
+++ gif_maker.py
@@ -0,0 +1,45 @@
+import os
+import json
+import requests
+from pandas.io.json import json_normalize
+from random import choice
+import logging
+import urllib
+import shutil
+import gearman
+class IntentParser(object):
+    def __init__(self):
+        self.header = {"Content-Type":"application/json","apiKey":"F9RpSEoojxWCec6"}
+        self.gm_worker = gearman.GearmanWorker(['localhost:5005'])
+        self.gm_worker.register_task('Gator',self.run)
+    def getUrlList(self,sticker):
+        list1 = []
+        for dictionary in sticker:
+            for imagetype in dictionary:
+                for each in dictionary[imagetype]:
+                    if str(each) == 'original':
+                        if '.png' in dictionary[imagetype][each]['url']:
+                            list1.append(dictionary[imagetype][each]['url'])
+        return list1
+
+    def run(self,gearman_worker,gearman_job):
+        body = {"sentence" : gearman_job.data}
+        result = requests.post('http://text2gif.guggy.com/v2/guggify',headers = self.header,json = body)
+        content = json.loads(result._content)
+        stickers = content['sticker']
+        urls = self.getUrlList(stickers)
+        if urls:
+            ph = str(choice(urls))
+            r = requests.get(ph, stream=True)
+            print r.status_code
+            if r.status_code == 200:
+                with open("img.png", 'wb') as f:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, f)
+        else:
+            print "Not Found"
+        return json.dumps({})
+
+if __name__ == '__main__':
+    intent = IntentParser()
+    intent.gm_worker.work()
---
+++ inboundtelegram.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater
+import logging
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import gearman
+import json
+AUTH_TOKEN = "285351123:AAFevc78WhbcwguBbOZY8MY09jS5l-NVb9E"
+# bot.sendPhoto(chat_id=chat_id,photo = open('img.png','rb'))
+class first_bot:
+    def __init__(self):
+        self.updater = Updater(token = AUTH_TOKEN )
+        self.dispatcher = self.updater.dispatcher
+        self.gm_client = gearman.GearmanClient(['localhost:4730'] )
+        logging.basicConfig( format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level = logging.INFO )
+
+    def run(self, bot_value, update_value):
+        global bot
+        global update
+        bot = bot_value
+        update = update_value
+        ChatMsg = update.message.text
+        completed_task = self.gm_client.submit_job('Gator',ChatMsg)
+        result = json.loads(completed_task.result)
+        if completed_task.state == 'COMPLETE':
+            bot_value.sendPhoto(chat_id=chat_id,photo = open('img.png','rb'))        
+
+if __name__ == '__main__':
+        telebot = first_bot()
+        echo_handler = MessageHandler([Filters.text],telebot.run)
+        telebot.dispatcher.add_handler(echo_handler)
+        telebot.updater.start_polling(poll_interval = 1.0)
+        telebot.updater.idle()
